7147bd51eed627aca70307a36efb36eccb989e32
Daniel Agus Sidabutar <aguspermadi94@gmail.com>
2019-05-09 10:52:22 +0700

    add wrapper for jwt module

---
+++ app/lib/json_web_token.rb
@@ -0,0 +1,22 @@
+class JsonWebToken
+  HMAC_SECRET = Rails.application.secrets.secret_key_base
+
+  def self.encode(payload, exp = 24.hours.from_now)
+    # set expiry to 24 hours from creation time
+    payload[:exp] = exp.to_i
+    #sign token with application secret
+    JWT.encode(payload, HMAC_SECRiET)
+  end
+
+  def self.decode(token)
+    body = JWT.decode(token, HMAC_SECRET)[0]
+    HashWithIndifferentAccess.new body
+
+    # rescue from all decode errors
+    rescue JWT::DecodeError => e
+
+    # raise custom error to be handled by custom handler
+    raise ExceptionHandler::InvalidToken, e.message
+  end
+
+end
